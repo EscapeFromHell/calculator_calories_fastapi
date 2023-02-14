a2d5e0605d25446fabf6b8db8d7e975815a7b93c
Пугачев Кирилл Николаевич <pugachevkn@svrauto.ru>
2023-02-14 16:30:34 +0400

    update project's structure

--- src/api/api_v1/endpoints/meal.py
+++ src/api/api_v1/endpoints/meal.py
@@ -1,7 +1,5 @@
-import asyncio
 from typing import Optional
 
-import httpx
 from fastapi import APIRouter, Query, Depends
 from sqlalchemy.orm import Session
 
@@ -11,7 +9,6 @@ from app.schemas.meal import Meal, MealCreate, MealSearchResults
 
 
 router = APIRouter()
-RECIPE_SUBREDDITS = ["recipes", "easyrecipes", "TopSecretRecipes"]
 
 
 @router.get("/search/", status_code=200, response_model=MealSearchResults)
@@ -44,28 +41,3 @@ def create_meal(
     meal = meal_crud.create(db=db, obj_in=meal_in)
 
     return meal
-
-
-async def get_reddit_top_async(subreddit: str) -> list:
-    async with httpx.AsyncClient() as client:
-        response = await client.get(
-            f"https://www.reddit.com/r/{subreddit}/top.json?sort=top&t=day&limit=5",
-            headers={"User-agent": "meal bot 0.1"},
-        )
-
-    subreddit_recipes = response.json()
-    subreddit_data = []
-    for entry in subreddit_recipes["data"]["children"]:
-        score = entry["data"]["score"]
-        title = entry["data"]["title"]
-        link = entry["data"]["url"]
-        subreddit_data.append(f"{str(score)}: {title} ({link})")
-    return subreddit_data
-
-
-@router.get("/ideas/async")
-async def fetch_ideas_async() -> dict:
-    results = await asyncio.gather(
-        *[get_reddit_top_async(subreddit=subreddit) for subreddit in RECIPE_SUBREDDITS]
-    )
-    return dict(zip(RECIPE_SUBREDDITS, results))
--- src/core/models/base.py
+++ src/core/models/base.py
@@ -2,7 +2,6 @@ import typing as t
 
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
-
 class_registry: t.Dict = {}
 
 
@@ -11,6 +10,7 @@ class Base:
     id: t.Any
     __name__: str
 
+    # Generate __tablename__ automatically
     @declared_attr
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
